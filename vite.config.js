b694615783385d39c8883ce8cff834504d34cfbc
anjupaka <anjupaka12@gmail.com>
2025-01-02 22:05:57 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import plugin from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
-    base: "/calendar_tracking_new/",
+    base: "/CalendarApplication_ENTNT/",
+    
+    build: {
+        chunkSizeWarningLimit: 3000, // Increase limit to 1MB
+        outDir: "dist",
+    },
         server: {
         port: 53332,
     }
