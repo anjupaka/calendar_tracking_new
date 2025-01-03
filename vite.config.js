20f22cbda7cd206a5fe46bb47406d1c574c72703
anjupaka <anjupaka12@gmail.com>
2025-01-03 15:40:52 +0530

    Third commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import plugin from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
-    base: "/calendar_tracking_new",
+    base: '/',
     
     build: {
         chunkSizeWarningLimit: 3000, // Increase limit to 1MB
